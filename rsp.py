ebf90f9e964540a5d902cb9701b8eec97395babb
Gordon Xie <z5160618@student.unsw.edu.au>
2020-02-24 09:50:54 -0500

    completed rock scissors paper game

--- rsp.py
+++ rsp.py
@@ -8,13 +8,24 @@ matchups = {'rock':'scissors',
         'scissors':'paper',
         'paper':'rock'}
 
-rng = rsp[random.randint(1,3)]
+losses = {'rock':'paper',
+        'scissors':'rock',
+        'paper':'scissors'}
+
 while True:
-    userinput = input('Please enter rock, scissors, or paper (To quit, enter ''q'' at any time):')
+    rng = rsp[random.randint(1,3)]
+    message = 'The computer has selected {}'.format(rng)
+    userinput = input('Please enter rock, scissors, or paper (To quit, enter ''q'' at any time): ').lower()
     if userinput == 'q':
         break
     elif userinput == matchups[rng]:
+        print(message)
         print('You lose!')
-    else:
-        print('Congratulations! Lets play again!')
-    
+    elif userinput == rng:
+        print(message)
+        print('Its a draw!')
+    elif userinput == losses[rng]:
+        print(message)
+        print('Congratulations, you win! Lets play again!')
+    else: 
+        print('Please insert a valid input')
