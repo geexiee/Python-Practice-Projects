edff80fed6af116f062eb3d444bfc58394ab6b30
Gordon Xie <z5160618@student.unsw.edu.au>
2020-02-24 20:17:53 -0500

    completed hangman game (minus drawing the picture)

---
+++ hangman.py
@@ -0,0 +1,36 @@
+import random
+
+with open("sowpods.txt", "r") as dictionary:
+    wordlist = list(dictionary)
+randomword = random.choice(wordlist).strip()
+count = 0
+wordlen = len(randomword)
+nguesses = wordlen+1
+intro = 'Welcome to hangman! Please enter individual characters to guess the word. This word has {} letters in it. You have {} guesses to figure out what the word is.'.format(wordlen,nguesses )
+print(intro)
+underscores = ''
+for i in randomword:
+    underscores += '_'
+print(underscores)
+
+while count < nguesses:
+    guess = input('Guess a letter: ')
+    if guess.isalpha() or len(guess) > 1:
+        if guess in randomword:
+            j = 0
+            while j < len(randomword):
+                if randomword[j] == guess:
+                    underscores = underscores[:j] + guess + underscores[j+1:]
+                j += 1
+        count += 1
+        remaining_guesses = nguesses - count
+        remaining_guesses_str = 'You have {} guesses remaining.'.format(remaining_guesses)
+        print(remaining_guesses_str)
+        print(underscores)
+        if '_' not in underscores:
+            print('Congratulations, you win!')
+            break
+        else:
+            print('Please enter a valid character')
+
+print('You lose :(')
